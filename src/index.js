9460178743c4fb4909a5efd6f3e59dd10d9b48de
Alexandr Kolos <colossila@gmail.com>
2024-08-13 00:09:17 +0300

    feat: decide reverse problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
-}
+    const strNum = n.toString();
+    const revStr = strNum.split('').reverse().join('');
+    const revNum = parseInt(revStr);
+    return revNum;
+};
